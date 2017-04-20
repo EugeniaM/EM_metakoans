30374218e730eaf70b126e307cbc137f72a14452
EugeniaM <maltsevae88@gmail.com>
2017-04-20 23:30:44 +0300

    Removed unnecessary self usage

--- knowledge.rb
+++ knowledge.rb
@@ -4,19 +4,19 @@ def attribute (arg, &block)
 		inst_var = arg.keys[0]
 	end
 
-	self.send :define_method, "#{inst_var}?" do
-		!!self.instance_variable_get("@#{inst_var}".to_sym)
+	define_method "#{inst_var}?" do
+		!!instance_variable_get("@#{inst_var}".to_sym)
 	end
 
-	self.send :define_method, "#{inst_var}" do
-		if !self.instance_variable_defined?("@#{inst_var}".to_sym)
+	define_method "#{inst_var}" do
+		if !instance_variable_defined?("@#{inst_var}".to_sym)
 			value = block ? (instance_eval &block) : (arg.is_a?(Hash) ? arg.values[0] : nil)
-			self.instance_variable_set("@#{inst_var}".to_sym, value)
+			instance_variable_set("@#{inst_var}".to_sym, value)
 		end
-		self.instance_variable_get("@#{inst_var}".to_sym)
+		instance_variable_get("@#{inst_var}".to_sym)
 	end
 
-	self.send :define_method, "#{inst_var}=" do |arg|
-		self.instance_variable_set("@#{inst_var}".to_sym, arg)
+	define_method "#{inst_var}=" do |arg|
+		instance_variable_set("@#{inst_var}".to_sym, arg)
 	end
 end
